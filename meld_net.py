4c519f698a71749c8994f07284298857e2fb20d9
Joaquin Casanova <joaquin.casanova@gmail.com>
2017-03-20 10:20:43 -0400

    fixed stddev in variable summaries

--- meld_net.py
+++ meld_net.py
@@ -28,9 +28,8 @@ def variable_summaries(var, name):
   with tf.name_scope('summaries'):
     mean = tf.reduce_mean(var)
     tf.summary.scalar('mean/' + name, mean)
-    with tf.name_scope('stddev'):
-      stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-      tf.summary.scalar('sttdev/' + name, stddev)
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('sttdev/' + name, stddev)
     tf.summary.scalar('max/' + name, tf.reduce_max(var))
     tf.summary.scalar('min/' + name, tf.reduce_min(var))
     tf.summary.histogram('histogram/'+name, var)
